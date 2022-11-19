ccb4ff4fed35a3689c02c449158dc9669de2844b
Paulo Renato <paulorenatorf@hotmail.com>
2022-11-19 15:43:17 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+const App = () => {
+  const uploadHandler = (event) => {
+    const file = event.target.files[0];
+    if (!file) return;
+
+    const formData = new FormData();
+    formData.append("newFile", file);
+
+    fetch("http://localhost:5000/uploadfile", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        console.log("Success:", result);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="file-card">
+        <div className="file-inputs">
+          <input type="file" name="file" onChange={uploadHandler} />
+          <button>Upload</button>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
